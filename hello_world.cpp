ac978abce0ca2c68d75bd0986e61c988c99fc2c6
ch1la_bmstu <a2007kov@gmail.com>
2026-04-06 23:24:21 +0300

    refactoring comments in main

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 // Программа для демонстрации персонализированного "Hello World"
 // Исправлено: удалено 'using namespace std' для большей ясности кода
-
+//programm for demostrating personalised hello world
 int main(){
 std::string name;
 std::cin >> name;
